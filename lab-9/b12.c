63cf0cc60505daaab0351292db08c3a6cbadf29d
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-02-27 18:06:25 +0300

    lab9 b12: update

--- lab-9/b12.c
+++ lab-9/b12.c
@@ -3,62 +3,56 @@
 #include <string.h> //strtok()
 
 void lab9_b12();
-void buble_sort(char *s);
-void clean_arr(char *s);
+void buble_sort(char*);
+void clean_arr(char*);
 
 int main() {
     lab9_b12();
+    printf("\n\n");
     return 0;
 }
 
 void lab9_b12() {
-    //создание строки в виде указателя и её ввод с клавиатуры
-    char *str = (char*) calloc(509, sizeof(char));
+    char* str = (char*) calloc(509, sizeof(char));
     printf("Write string:\n");
     gets(str);
-    
-    char words[509][509]; //массив для слов со строки
-    int kWords = 0; //число слов
-    char letters[509]; //массив для 1-ых букв
-    
-    //разделение строки на слова
-    //и взятие у первого слова 1-ого символа
-    char *pointerStr = strtok(str, " ");
-    
+
+
+    char words[509][509]; //array for words
+    int numWords = 0; //number of words
+    char* letters = (char*) calloc(509, sizeof(char)); //array for 1st letters in words
+
+
+    char* pointerStr = strtok(str, " ");
     while(pointerStr) {
-    	//printf("%s\n", pointerStr);
-    	
-    	strcpy(words[kWords], pointerStr); //записать слово из указателя в массив со словами
-    	//printf("%c\n\n", words[i][0]);
-    	
-    	letters[kWords] = words[kWords][0]; //записать первую букву слова в массив со словами
-    	kWords++; //слов стало на 1 больше
-    	
+    	strcpy(words[numWords], pointerStr); //input word in array words
+    	letters[numWords] = words[numWords][0]; //1st letter in string letters
+    	numWords++;
     	pointerStr = strtok(NULL, " ");
 	}
-    
-	for (int i = 0; i < kWords; i++)
-		printf("%s\n", words[i]);
-	
-	printf("\n\nfirst letters:\n%s\n\n", letters);
-	buble_sort(letters); //сортировка слов по алфавиту
-	
-	printf("\n\nletters:\n%s\n\n", letters);
-	//функция удаления букв
-	//удалит символ, если он один
-	//оставит символ, если их больше 2-ух, но удалит, его копии
+	free(str);
+
+	buble_sort(letters); //sort the letters alphabetically
+
+
+	//function that removes letters
+	//deletes a character if it is 1
+	//will leave a symbol if there are more than 2, but will delete its copies
     clean_arr(letters);
-	
-  	for (int i = 0; i < kWords; i++)
+
+
+	printf("\noutput of words whose first letter occurs more than 1 time\n");
+  	for (int i = 0; i < numWords; i++)
   		for (int j = 0; j < strlen(letters); j++)
   			if (words[i][0] == letters[j]) {
   				printf("%s ", words[i]);
   				break;
 			}
 
+    free(letters);
 }
 
-void buble_sort(char *s) {
+void buble_sort(char* s) {
     for (int i = 0; i < strlen(s); i++)
         for (int j = 0; j < strlen(s); j++)
             if (s[j] > s[i]) {
@@ -68,7 +62,7 @@ void buble_sort(char *s) {
             }
 }
 
-void clean_arr(char *s) {
+void clean_arr(char* s) {
     for (int i = 0; i < strlen(s); i++)
         if (s[i] != s[i + 1])
             s[i] = ' ';
@@ -79,6 +73,4 @@ void clean_arr(char *s) {
                 s[j] = s[j + 1];
             i--;
         }
-
-    printf("clean arr:\n%s\n\n\n", s);
 }
