e30cd13983728d405e1eb95a83b11d084a59c677
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-03-11 23:12:07 +0300

    update lab 2020-03-06: a25

---
+++ 2020-03-06_lab/a25/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void lab();
+void Transp(int**, int);
+
+int main(int argc, char *argv[])
+{
+	lab();
+}
+
+void lab() {
+
+	int n;
+	printf("n := ");
+	scanf("%d", &n);
+
+
+	int** a = (int**) calloc(n, sizeof(int*));
+	for  (int i = 0; i < n; i++) {
+		a[i] = (int*) calloc(n, sizeof(int));
+	}
+
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			a[i][j] = rand() % 9 + 1;
+			printf("%2d", a[i][j]);
+		}
+		printf("\n");
+	}
+
+
+	Transp(a, n);
+
+
+	printf("\n\nTransported matrix:\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%2d", a[i][j]);
+		}
+		printf("\n");
+	}
+
+}
+
+void Transp(int** A, int M) {
+	int k = 0;
+	for (int i = 0; i < M - 1; i++) {
+		for (int j = k; j < M; j++) {
+			int temp = A[i][j];
+			A[i][j] = A[j][i];
+			A[j][i] = temp;
+		}
+		k++;
+	}
+}
