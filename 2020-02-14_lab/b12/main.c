5c288ae37007ac35bda11b88ae334fae8b70caf9
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-03-12 19:16:47 +0300

    update lab 2020-02-14: b12

--- 2020-02-14_lab/b12/main.c
+++ 2020-02-14_lab/b12/main.c
@@ -1,76 +1,23 @@
-#include <stdio.h> //gets() printf()
-#include <stdlib.h> //calloc() free()
-#include <string.h> //strtok() strcpy() strlen()
-
-void lab9_b12();
-void buble_sort(char*);
-void clean_arr(char*);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-    lab9_b12();
-    printf("\n\n");
-    return 0;
-}
-
-void lab9_b12() {
     char* str = (char*) calloc(509, sizeof(char));
-    printf("Write string:\n");
+    printf("Input a few words\n");
     gets(str);
 
-
-    char words[509][509]; //array for words
-    int numWords = 0; //number of words
-    char* letters = (char*) calloc(509, sizeof(char)); //array for 1st letters in words
-
-
-    char* pointerStr = strtok(str, " ");
-    while(pointerStr) {
-    	strcpy(words[numWords], pointerStr); //input word in array words
-    	letters[numWords] = words[numWords][0]; //1st letter in string letters
-    	numWords++;
-    	pointerStr = strtok(NULL, " ");
-	}
-	free(str);
-
-	buble_sort(letters); //sort the letters alphabetically
-
-
-	//function that removes letters
-	//deletes a character if it is 1
-	//will leave a symbol if there are more than 2, but will delete its copies
-    clean_arr(letters);
-
-
-	printf("\noutput of words whose first letter occurs more than 1 time\n");
-  	for (int i = 0; i < numWords; i++)
-  		for (int j = 0; j < strlen(letters); j++)
-  			if (words[i][0] == letters[j]) {
-  				printf("%s ", words[i]);
-  				break;
-			}
-
-    free(letters);
-}
-
-void buble_sort(char* s) {
-    for (int i = 0; i < strlen(s); i++)
-        for (int j = 0; j < strlen(s); j++)
-            if (s[j] > s[i]) {
-                char t = s[j];
-                s[j] = s[i];
-                s[i] = t;
+    char* pointer = strtok(str, " ");
+    while(pointer) {
+        for (int i = 1; i < strlen(pointer); i++) {
+            if (pointer[0] == pointer[i]) {
+                printf("%s ", pointer);
+                break;
             }
-}
-
-void clean_arr(char* s) {
-    for (int i = 0; i < strlen(s); i++)
-        if (s[i] != s[i + 1])
-            s[i] = ' ';
-
-    for (int i = 0; i < strlen(s); i++)
-        if (s[i] == s[i + 1] || s[i] == ' ') {
-            for (int j = i; j < strlen(s); j++)
-                s[j] = s[j + 1];
-            i--;
         }
+        pointer = strtok(NULL, " ");
+    }
+
+    free(str);
+    return 0;
 }
