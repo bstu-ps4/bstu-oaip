e8318e4acfa1d612f25f451e4a0a7cbcaf1bef7f
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-04-18 14:07:58 +0300

    update lab 10 c: ifndef define endif

--- labs/10/c/7/inc/Combin2.h
+++ labs/10/c/7/inc/Combin2.h
@@ -1,3 +1,7 @@
 #pragma once
+#ifndef _COMBIN2_H_
+#define _COMBIN2_H_
 
 int Combin2(int, int);
+
+#endif
--- labs/10/c/7/inc/lab.h
+++ labs/10/c/7/inc/lab.h
@@ -1,7 +1,11 @@
 #pragma once
+#ifndef _LAB_H_
+#define _LAB_H_
 
 #include <stdio.h>
 
 #include "../inc/Combin2.h"
 
 void lab();
+
+#endif
