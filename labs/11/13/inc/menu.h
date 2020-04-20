580c9c3eab70b738856f46f7c9652bafaa33897a
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-04-20 14:20:35 +0300

    lab 11: module menu

---
+++ labs/11/13/inc/menu.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef _MENU_H_
+#define _MENU_H_
+    #include <stdio.h>
+
+    #include "clearConsole.h"
+    #include "getch.h"
+    #include "lab.h"
+
+    //void menu(int ID, struct structure_for_car* cars_data);
+#endif
