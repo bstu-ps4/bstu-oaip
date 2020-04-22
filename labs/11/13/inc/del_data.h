d7f86915f450ce4e80a79c68bff4629cc7e12091
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-04-22 23:53:56 +0300

    lab 11: delete_data

--- labs/11/13/inc/del_data.h
+++ labs/11/13/inc/del_data.h
@@ -1,6 +1,8 @@
 #pragma once
 #ifndef _DEL_DATA_H_
 #define _DEL_DATA_H_
+    #include <stdio.h>
+
     #include "lab.h"
     #include "clearConsole.h"
     #include "pause_console.h"
--- labs/11/13/src/del_data.c
+++ labs/11/13/src/del_data.c
@@ -4,6 +4,18 @@ void del_data(int ID, struct structure_for_car* cars_data)
 {
     clearConsole();
 
-    pause_console();
+    int delete_ID;
+    printf("Какой элемент удалить: ");
+    scanf("%d", &delete_ID);
+
+    if (delete_ID < ID)
+    {
+        ID--;
+        for (int i = delete_ID; i < ID; i++)
+        {
+            cars_data[i] = cars_data[i + 1];
+        }
+    }
+
     menu(ID, cars_data);
 }
