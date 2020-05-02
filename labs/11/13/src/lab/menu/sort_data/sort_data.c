b928c7e9e3b2c505a2e41cf94cc8e5b4da3d37a6
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-05-02 15:28:45 +0300

    lab 11: update structure sort_data

--- labs/11/13/src/lab/menu/sort_data/sort_data.c
+++ labs/11/13/src/lab/menu/sort_data/sort_data.c
@@ -36,19 +36,3 @@ void sort_data(int ID, struct structure_for_car* cars_data)
 
     menu(ID, cars_data);
 }
-
-void get_sorted_array(
-    int ID,
-    struct structure_for_car* cars_data,
-    void (*sub_foo)(int ID, struct structure_for_car* cars_data, int i, int j, int k)
-)
-{
-    for (int i = 0; i < ID; i++)
-    {
-        for (int j = 0; j < ID; j++)
-        {
-            int k = 0;
-            sub_foo(ID, cars_data, i, j, k);
-        }
-    }
-}
--- labs/11/13/src/lab/menu/sort_data/sort_data.h
+++ labs/11/13/src/lab/menu/sort_data/sort_data.h
@@ -5,11 +5,11 @@
     #include "../../../my_libs/clearConsole/clearConsole.h"
     #include "../../../my_libs/getch/getch.h"
     #include "../menu.h"
-    #include "sort_data_in_number_field/sort_data_in_number_field.h"
-    #include "sort_data_by_mark_field/sort_data_by_mark_field.h"
-    #include "sort_data_by_surname_field/sort_data_by_surname_field.h"
+    #include "get_sorted_array/get_sorted_array.h"
+    #include "get_sorted_array/sort_data_in_number_field/sort_data_in_number_field.h"
+    #include "get_sorted_array/sort_data_by_mark_field/sort_data_by_mark_field.h"
+    #include "get_sorted_array/sort_data_by_surname_field/sort_data_by_surname_field.h"
     #include "sort_data_by_osmotr_field/sort_data_by_osmotr_field.h"
 
     void sort_data(int, struct structure_for_car*);
-    void get_sorted_array(int, struct structure_for_car*, void (*)(int, struct structure_for_car*, int, int, int));
 #endif
