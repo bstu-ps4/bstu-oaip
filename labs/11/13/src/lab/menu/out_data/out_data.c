02cfe2f53235d1fef2ecce20e249b39ef6f4882b
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-05-02 13:27:48 +0300

    lab 11: fix out osmotr data

--- labs/11/13/src/lab/menu/out_data/out_data.c
+++ labs/11/13/src/lab/menu/out_data/out_data.c
@@ -17,7 +17,7 @@ void out_data(int ID, struct structure_for_car* cars_data)
         printf("%s\t", cars_data[i].number);
         printf("%s\t", cars_data[i].mark);
         printf("%s\t", cars_data[i].surname);
-        printf("%s\t", (cars_data[i].osmotr == '1'? "Пройден": "Не пройден"));
+        printf("%s\t", (cars_data[i].osmotr == yes? "Пройден": "Не пройден"));
         printf("\n");
     }
     pause_console();
