eba365bc01dddc55252fa6c2ce609c4833df89c9
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-05-03 22:57:34 +0300

    lab 11: update out table

--- labs/11/13/src/lab/menu/out_data/out_data.c
+++ labs/11/13/src/lab/menu/out_data/out_data.c
@@ -4,20 +4,30 @@ void out_data(int ID, struct structure_for_car* cars_data)
 {
     clearConsole();
 
-    printf("ID\t");
-    printf("Номер\t");
-    printf("Марка\t");
-    printf("Фамилия\t");
-    printf("Осмотр\t");
+    printf("|");
+    printf(" ID   |");
+    printf(" Номер      |");
+    printf(" Марка      |");
+    printf(" Фамилия              |");
+    printf(" Осмотр     |");
+    printf("\n");
+
+    printf("|");
+    printf(" ---- |");
+    printf(" ---------- |");
+    printf(" ---------- |");
+    printf(" -------------------- |");
+    printf(" ---------- |");
     printf("\n");
 
     for (int i = 0; i < ID; i++)
     {
-        printf("%d\t", i);
-        printf("%s\t", cars_data[i].number);
-        printf("%s\t", cars_data[i].mark);
-        printf("%s\t", cars_data[i].surname);
-        printf("%s\t", (cars_data[i].osmotr == yes? "Пройден": "Не пройден"));
+        printf("|");
+        printf(" %-4d |", i);
+        printf(" %-10s |", cars_data[i].number);
+        printf(" %-10s |", cars_data[i].mark);
+        printf(" %-20s |", cars_data[i].surname);
+        printf(" %s |", (cars_data[i].osmotr == yes? "Пройден   ": "Не пройден"));
         printf("\n");
     }
     pause_console();
