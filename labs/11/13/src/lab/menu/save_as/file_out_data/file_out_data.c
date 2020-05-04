4d0f67021675c97b931604fb0d0332cba5dd09d0
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-05-04 16:24:23 +0300

    lab 11/12: file_out_data

--- labs/11/13/src/lab/menu/save_as/file_out_data/file_out_data.c
+++ labs/11/13/src/lab/menu/save_as/file_out_data/file_out_data.c
@@ -6,26 +6,18 @@ void file_out_data(int ID, struct structure_for_car* cars_data, char* file_name,
 
     for (int i = 0; i < ID; i++)
     {
-        fprintf(file_pointer, "%d", i);
-        fprintf(file_pointer, "\t");
+        fprintf(file_pointer, "%d\t", i);
 
-        fprintf(file_pointer, "%d", cars_data[i].number_size);
-        fprintf(file_pointer, "\t");
-        fprintf(file_pointer, "%s", cars_data[i].number);
-        fprintf(file_pointer, "\t");
+        fprintf(file_pointer, "%d\t", cars_data[i].number_size);
+        fprintf(file_pointer, "%s\t", cars_data[i].number);
 
-        fprintf(file_pointer, "%d", cars_data[i].mark_size);
-        fprintf(file_pointer, "\t");
-        fprintf(file_pointer, "%s", cars_data[i].mark);
-        fprintf(file_pointer, "\t");
+        fprintf(file_pointer, "%d\t", cars_data[i].mark_size);
+        fprintf(file_pointer, "%s\t", cars_data[i].mark);
 
-        fprintf(file_pointer, "%d", cars_data[i].surname_size);
-        fprintf(file_pointer, "\t");
-        fprintf(file_pointer, "%s", cars_data[i].surname);
-        fprintf(file_pointer, "\t");
+        fprintf(file_pointer, "%d\t", cars_data[i].surname_size);
+        fprintf(file_pointer, "%s\t", cars_data[i].surname);
 
-        fprintf(file_pointer, "%d", (cars_data[i].osmotr == yes? yes: no));
-        fprintf(file_pointer, "\n");
+        fprintf(file_pointer, "%d\n", cars_data[i].osmotr == yes? yes: no);
     }
 
     fclose(file_pointer);
