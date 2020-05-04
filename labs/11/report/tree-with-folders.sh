c7af9e4738b22b275a7e4dad3caad10d17d26456
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-05-04 18:38:37 +0300

    lab 11/12: tree

---
+++ labs/11/report/tree-with-folders.sh
@@ -0,0 +1,2 @@
+cd ../../../;
+tree -I "10|old|*.o|*.d|*.c|*.h|*.tex|*.png|*.tsv|*.sh|*.drawio|*.sty|*.aux|*.fdb_latexmk|*.fls|*.log|*.pdf|*.synctex.gz|*.synctex(busy)|*.toc"
---
+++ labs/11/report/tree.sh
@@ -0,0 +1,2 @@
+cd ../../../;
+tree -I "10|old|*.o|*.d|*.aux|*.fdb_latexmk|*.fls|*.log|*.pdf|*.synctex.gz|*.synctex(busy)|*.toc"
