edbd3133962d1ebbdfc11f09ac03d2d4d68fbf62
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-05-05 20:28:00 +0300

    lab 11/12: update reports

--- labs/11/report/tree-with-folders.sh
+++
@@ -1,2 +0,0 @@
-cd ../../../;
-tree -I "10|old|*.o|*.d|*.c|*.h|*.tex|*.png|*.tsv|*.sh|*.drawio|*.sty|*.aux|*.fdb_latexmk|*.fls|*.log|*.pdf|*.synctex.gz|*.synctex(busy)|*.toc"
--- labs/11/report/tree.sh
+++
@@ -1,2 +0,0 @@
-cd ../../../;
-tree -I "10|old|*.o|*.d|*.aux|*.fdb_latexmk|*.fls|*.log|*.pdf|*.synctex.gz|*.synctex(busy)|*.toc"
